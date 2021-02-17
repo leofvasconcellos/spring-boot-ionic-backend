3996d0c5a1877b5cd8d890e851206f0ebbfbb4de
Leandro Farias <leofvasconcellos@gmail.com>
2021-02-17 11:13:32 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/leandro/cursomc/services/ClienteService.java
@@ -0,0 +1,32 @@
+package com.leandro.cursomc.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leandro.cursomc.domain.Cliente;
+import com.leandro.cursomc.repositories.ClienteRepository;
+import com.leandro.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Optional<Cliente> buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return Optional.ofNullable(obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())));
+	}
+	
+	public List<Cliente> listar() {
+		List<Cliente> lista = new ArrayList<>();
+		lista = repo.findAll();		
+		
+		return lista;
+	}
+}
